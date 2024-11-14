e03b2490505459291f4f5f0120911c95f3184868
Maxim <142741515+SonOfBaphomet666@users.noreply.github.com>
2024-11-14 22:22:56 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Navbar from "./components/navbar/navbar";
+import "./main.scss";
+
+function App() {
+  return <Navbar />;
+}
+
+export default App;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <>
+        <div className="cont">
+asdasdasdasd
+        </div>
+        </>
+    )
+};
+export default Navbar;
